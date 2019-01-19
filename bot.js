61417953b5cc76b5918b43061f9139e16418c47d
illusiveo <46373282+illusiveo@users.noreply.github.com>
2019-01-19 15:20:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,7 +88,7 @@ client.on('message', async msg => {
                     .setDescription(`**الرجاء إختيار رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
  
-                    .setFooter("Some Problem? contact Developer! : Da Shine#0022")
+                    .setFooter("")
                     msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
                    
                     // eslint-disable-next-line max-depth
